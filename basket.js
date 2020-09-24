99466cae52db48d8dbafb6ef6f8907b9bb64d67c
Евгения Масликова < >
2020-09-24 11:02:22 +0300

    first show

--- basket.js
+++ basket.js
@@ -1,4 +1,3 @@
-'use strict';
 const wordbasket_start = document.getElementById('wordbasket_start');
 const wordbasket_left = document.getElementById('wordbasket_left');
 const wordbasket_right = document.getElementById('wordbasket_right');
@@ -12,53 +11,57 @@ let words_basket_array = [
   ['Passion', 'Fashion', 'Fassion' , 'Pession', 'Psasion' ],
   ['Sustainability', '5ustionability', 'Sustianability' , 'Sostatinablity', 'Sustainablity' ]
 ]; 
-let track = document.getElementsByClassName('track');
+let play_array;
+let wrong_number = 1;
+var b = wordBasket.getBoundingClientRect();  
+var bx = gameBasketBox.getBoundingClientRect();
+var x = bx.width / 5;
+var track = document.getElementsByClassName('track');
 let score = 0;
-let word_count =1;
-let max_word_count =  words_basket_array.length;
-let corect = ['corect','wrong'];
-let b_coordinate = wordBasket.getBoundingClientRect();
-let bx_basket = gameBasketBox.getBoundingClientRect();
-let x_basket = bx_basket.width / 5; 
+var good = ["good","bad"];
 
+// document.getElementById('score').innerHTML = score;
+
+// build tree and game layout
 function buildGame() {  
-  for(let i=0; i< track.length; i++){
+  for(var i=0;i<track.length;i++){
   
   function buildBlocks(){
-    let block = document.createElement('div');
-    block.className = 'block';
-    let falling_word = document.createElement('div');
-    falling_word.className = 'falling_word';
+    var block = document.createElement('div');
+    block.className = "block";
+    var fruit = document.createElement('div');
+    fruit.className = "fruit orange ghost";
     
-    track[i].appendChild(block).appendChild(falling_word)
+    track[i].appendChild(block).appendChild(fruit)
   }
-    buildBlocks()
-    buildBlocks()
-    buildBlocks()
-    buildBlocks()    
-    buildBlocks()    
+    buildBlocks();
+    buildBlocks();
+    buildBlocks();
+    buildBlocks();    
+    buildBlocks();    
   }
 }
 
 
 function over(){
-    let f = document.querySelectorAll('.top')
-    for(let j=0;j<f.length;j++) {
+    var f = document.querySelectorAll('.top');
+    for(var j=0;j<f.length;j++) {
       f[j].remove(f[j]);
     }
   }  
 
 function moveFruit() {
-  if(score == 25) {
+  if(score == 5) {
     score = 0;
-    // document.getElementById('score').innerHTML = score;
-    over();       
-    // clearInterval(runGame);
+     // document.getElementById('score').innerHTML = score;
+         
+    clearInterval(runGame);
+    over();  
     game_result.style.display = 'block';
     game_result.innerText = 'Great job!';          
 
   }
-  if(score == -10) {
+  if(score == -20) {
 
     game_result.style.display = 'block';
     game_result.innerText = 'You made ' + score + ' mistakes. You can try again!';
@@ -67,95 +70,109 @@ function moveFruit() {
     score = 0;
     // document.getElementById('score').innerHTML = score
             
-    // clear falling_word for next game
+    // clear fruit for next game
     over();        
-    // clearInterval(runGame);  
-   
+    clearInterval(runGame);  
+    // alert('YOU LOSE!\r\rBE SAD.  :(')   
   }  
   
-  let falling_word = document.querySelectorAll('.top');
-  for(let i=0;i< falling_word.length; i++) {
-    let dest = falling_word[i].parentElement.nextSibling;
+  var fruit = document.querySelectorAll('.top');
+  for(var i=0;i< fruit.length; i++) {
+    var dest = fruit[i].parentElement.nextSibling;
     
-    // scoring and removing falling_word
+    // scoring and removing fruit
     if(!dest){
-      let block = falling_word[i].parentElement;
-      let wordBasket = document.getElementById('wordBasket');
+      var block = fruit[i].parentElement;
+      var wordBasket = document.getElementById('wordBasket');
       
       if(Math.floor(block.offsetLeft) == Math.floor(wordBasket.offsetLeft)) {
-        if(falling_word[i].classList.contains('correct')){
+        if(fruit[i].classList.contains("good")){
           score++;
-          // document.getElementById('score').innerHTML = score;
-          falling_word[i].remove(falling_word[i]);          
+           // document.getElementById('score').innerHTML = score;
+          fruit[i].remove(fruit[i]);          
         } else {
           score--;
-          // document.getElementById('score').innerHTML = score
-          falling_word[i].remove(falling_word[i]);
+           // document.getElementById('score').innerHTML = score
+          fruit[i].remove(fruit[i]);
         }
         
       } else {
-        if(falling_word[i].classList.contains('correct')){
+        if(fruit[i].classList.contains("good")){
             score--;
-            // document.getElementById('score').innerHTML = score        
+             // document.getElementById('score').innerHTML = score        
         }
-            falling_word[i].remove(falling_word[i]);                
+            fruit[i].remove(fruit[i]);                
       }      
     } else {
-      dest.appendChild(falling_word[i]);
+      dest.appendChild(fruit[i]);
     }    
   }
   
-  // add new falling_word
-  falling_word = document.createElement('div');
+  // add new fruit
+  var fruit = document.createElement('div');
+
+  let current_array = play_array.length;
+
+  let word_index = Math.floor(Math.random()*current_array); 
+
+   // 
+
+  if(current_array==0){
+     // alert("Все уже сделано");
+   }
 
 
 
-  let correctness = (Math.random() < 0.5 ? 'correct' : 'wrong');
+  let correctness = (Math.random() < 0.5 ? "good" : "bad");
 
-  falling_word.className = 'falling_word  ' +   ' word_view  '   + '  '  + correctness + ' ' + 'top';
+  fruit.className = "fruit apple " +  correctness + " top";
 
-  if(correctness == 'correct'){
-    falling_word.innerText = words_basket_array[0][0];
+  if(correctness == "good"){
+    fruit.innerText = words_basket_array[word_index][0];
   }
   else{
-    falling_word.innerText = words_basket_array[1][2];
+    
+    fruit.innerText = words_basket_array[word_index][wrong_number];
+    ++wrong_number;
+
+    if(wrong_number==4){
+      play_array.splice(word_index, 1);
+      wrong_number =1;
+    }
   }
 
-    
+   
 
 
-
-  // random falling_word color
-// let colorOne = 'hsla(' + (Math.floor(Math.random()*360)) + ', 100%, 50%, 1)';
-// let colorTwo = 'hsla(' + (Math.floor(Math.random()*360)) + ', 100%, 50%, 1)';
-// falling_word.style.background = "radial-gradient(circle at 60% 25%,"+colorOne+","+colorTwo+")"
-    track[Math.floor(Math.random()*track.length)].firstChild.appendChild(falling_word)
+    track[Math.floor(Math.random()*track.length)].firstChild.appendChild(fruit)
 }
 
-    let runGame = setInterval(moveFruit,  600);
+    var runGame = setInterval(moveFruit,  800);
 
 // click a start button
 wordbasket_start.addEventListener('click', function(e){
   game_result.style.display = 'none';
   over(); 
   buildGame();
+
+  play_array = words_basket_array;
 });
 
 function getcoordinates(){
-  b_coordinate = wordBasket.getBoundingClientRect();  
-  bx_basket = gameBasketBox.getBoundingClientRect();
-  x_basket = bx_basket.width / 5;
+ b = wordBasket.getBoundingClientRect();  
+ bx = gameBasketBox.getBoundingClientRect();
+ x = bx.width / 5;
 }
 
 
 // moving basket from keyboard;
 window.addEventListener('keyup',function(e){
-  b_coordinate = wordBasket.getBoundingClientRect();
-  bx_basket = gameBasketBox.getBoundingClientRect();
-  x_basket = bx_basket.width / 5;    
+  b = wordBasket.getBoundingClientRect();
+  bx = gameBasketBox.getBoundingClientRect();
+  x = bx.width / 5;    
   if(e.keyCode == 37) {
-    if(b_coordinate.left - x_basket > 0) {
-      wordBasket.style.left = wordBasket.offsetLeft - x_basket + 'px';  
+    if(b.left - x > 0) {
+      wordBasket.style.left = wordBasket.offsetLeft - x + 'px';  
     }
     else {
       wordBasket.style.left = '0';      
@@ -163,8 +180,8 @@ window.addEventListener('keyup',function(e){
   }
 
   if(e.keyCode == 39) {
-    if(wordBasket.offsetLeft + b_coordinate.width < bx_basket.width) {
-      wordBasket.style.left = wordBasket.offsetLeft + x_basket + 'px'; 
+    if(wordBasket.offsetLeft + b.width < bx.width) {
+      wordBasket.style.left = wordBasket.offsetLeft + x + 'px'; 
     }    
   }
 });
@@ -172,8 +189,8 @@ window.addEventListener('keyup',function(e){
 // moving basket using arrows buttons
 wordbasket_left.addEventListener('click', function(e){
    getcoordinates();
-    if(b_coordinate.left - x_basket > 0) {
-      wordBasket.style.left = wordBasket.offsetLeft - x_basket + 'px'  
+    if(b.left - x > 0) {
+      wordBasket.style.left = wordBasket.offsetLeft - x + 'px';  
     }
     else {     
        wordBasket.style.left = '0';      
@@ -181,7 +198,7 @@ wordbasket_left.addEventListener('click', function(e){
 });
 wordbasket_right.addEventListener('click', function(e){
  getcoordinates();    
-     if(wordBasket.offsetLeft + b_coordinate.width < bx_basket.width) {      
-      wordBasket.style.left = wordBasket.offsetLeft + x_basket + 'px';          
+     if(wordBasket.offsetLeft + b.width < bx.width) {      
+      wordBasket.style.left = wordBasket.offsetLeft + x + "px";          
     }    
 });
